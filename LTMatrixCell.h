912642f8176e7b1c5bbda01975926a461e1b5879
chenhaonan <chenhaoing@gmail.com>
2013-01-18 14:29:49 +0800

    Update LTMatrixCell.h

--- LTMatrixCell.h
+++ LTMatrixCell.h
@@ -1,27 +1,20 @@
 
 
 #import <UIKit/UIKit.h>
-#import "LTRatingView.h"
-#import "LTUIButton.h"
-#import "StyleLabel.h"
 
 @interface LTMatrixCell : UITableViewCell {
     UIImageView *_iconBorder;
     UIImageView *_thumbnail;
     UILabel *_stateLabel;
-    LTUIButton *_detailButton;
     UILabel *_titleLabel;
     UIImageView *_pickFrame;
-    StyleLabel *_chartLabel;
     UILabel *_maskLabel;
 }
 @property (nonatomic, retain) UIImageView *iconBorder;
 @property (nonatomic, retain) UIImageView *thumbnail;
 @property (nonatomic, retain) UILabel *stateLabel;
-@property (nonatomic, retain) LTUIButton *detailButton;
 @property (nonatomic, retain) UILabel *titleLabel;
 @property (nonatomic, retain) UIImageView *pickFrame;
-@property (nonatomic, retain) StyleLabel *chartLabel;
 @property (nonatomic, retain) UILabel *maskLabel;
 
 - (id)initWithStyle:(UITableViewCellStyle)style
