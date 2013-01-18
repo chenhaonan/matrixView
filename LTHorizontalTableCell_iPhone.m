dea728278352e0f8545adc7ba11f74984f31b63c
chenhaonan <chenhaoing@gmail.com>
2013-01-17 22:10:32 -0800

    Create LTHorizontalTableCell_iPhone.m

---
+++ LTHorizontalTableCell_iPhone.m
@@ -0,0 +1,25 @@
+//
+
+//
+
+#import "LTHorizontalTableCell_iPhone.h"
+
+@implementation LTHorizontalTableCell_iPhone
+
+@synthesize horizontalRowNumber;
+
+- (id)initWithFrame:(CGRect)frame
+{
+    if ((self = [super initWithFrame:frame]))
+    {
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
